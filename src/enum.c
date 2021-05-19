7928da07cb3f50e62e9e8ec7e97a83297c03a6a6
Mattherix <mattherix@protonmail.com>
2021-05-19 15:30:26 +0200

    Ajout de get_side_color()

--- src/enum.c
+++ src/enum.c
@@ -1,10 +1,33 @@
-//
-// Created by matthieu on 14/05/2021.
-//
 #include "enum.h"
 #include "colors.h"
 
 int select_color(T_COLOR color) {
+    return color;
+}
+T_COLOR get_side_color(T_SIDE side) {
+    T_COLOR color;
+    switch (side) {
+        case FRONT:
+            color = W;
+            break;
+        case BACK:
+            color = Y;
+            break;
+        case UP:
+            color = B;
+            break;
+        case DOWN:
+            color = G;
+            break;
+        case RIGHT:
+            color = R;
+            break;
+        case LEFT:
+            color = O;
+            break;
+        default:
+            color = LG;
+    }
     return color;
 }
 int side_to_index(T_SIDE side, Face* rubiks[6]) {
--- src/enum.h
+++ src/enum.h
@@ -10,6 +10,7 @@
 #include "main.h"
 
 int select_color(T_COLOR color);
+T_COLOR get_side_color(T_SIDE side);
 int side_to_index(T_SIDE side, Face* rubiks[6]);
 
 #endif //PROJECT_C_ENUM_H
